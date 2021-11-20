e4c202a7a5cf26d2a60d5fbd486f17d7cf6aa897
izaacyo <mariusppetrut@gmail.com>
2021-11-21 00:40:29 +0200

    if condition

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-let circles = [];
+let node = document.getElementById("node")
+let button = document.getElementsByClassName("button")
+let nodesContainer = document.querySelector("#node")
+
+console.log(node)
+console.log(button)
+console.log(nodesContainer)
+
+function Node(id, title, description) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.relations = [{
+        button: 1,
+        connection: "",
+        title: ""
+    }, {
+        button: 2,
+        connection: "",
+        title: ""
+    }, {
+        button: 3,
+        connection: "",
+        title: ""
+    }, {
+        button: 4,
+        connection: "",
+        title: ""
+    }]
+}
+
+let nodes = [];
+let relations = []
+
+
+const addNode = (e) => {
+    e.preventDefault();
+
+    if (nodes < 4) {
+        const newNode = document.createElement("div")
+        newNode.className = "container";
+        nodesContainer.appendChild(newNode);
+        nodes.push(newNode)
+    }
+
+}
+
+
+
 
 function setup() {
     for (let i = 0; i < 1; i++) {
@@ -23,28 +71,7 @@ function deleteNode() {
 }
 
 
-function Node(id, title, description, relations) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.relations = [{
-        button: 1,
-        connection: "",
-        title: ""
-    }, {
-        button: 2,
-        connection: "",
-        title: ""
-    }, {
-        button: 3,
-        connection: "",
-        title: ""
-    }, {
-        button: 4,
-        connection: "",
-        title: ""
-    }]
-}
+
 
 
 
