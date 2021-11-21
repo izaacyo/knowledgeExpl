0cfd0c242a0e1b6107f2d8b31332dc711671aa0f
izaacyo <mariusppetrut@gmail.com>
2021-11-21 13:22:33 +0200

    addNode

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
-/*let button = document.getElementsByClassName("button")
 let nodesContainer = document.querySelector("#area")
-let node = document.getElementById("container")
-let t = document.getElementById('target')*/
-
-let nodes = []
 
 
-class Node {
-    constructor(id, title, description, button, connection, title) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.relations = [{
-            button: button,
-            connection: connection,
-            title: title
-        }, {
-            button: 2,
-            connection: id,
-            title: ""
-        }, {
-            button: 3,
-            connection: id,
-            title: ""
-        }, {
-            button: 4,
-            connection: id,
-            title: ""
-        }]
-    }
+function Node(id, title, description, button1, button2, button3, button4, connection, relationTitle) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.relations = [{
+        button: button1,
+        connection: connection,
+        title: relationTitle
+    }, {
+        button: button2,
+        connection: id,
+        title: relationTitle
+    }, {
+        button: button3,
+        connection: id,
+        title: relationTitle
+    }, {
+        button: button4,
+        connection: id,
+        title: relationTitle
+    }]
 
 }
 
+let nodes = [];
 
-for (let i = 0; i < 5; i++) {
+let id = document.getElementById("circleId")
+
+let title = document.getElementById("title")
+
+let description = document.getElementById("description")
+
+
+let button1 = document.getElementById("input1").value
+let button2 = document.getElementById("input2").value
+let button3 = document.getElementById("input3").value
+let button4 = document.getElementById("input4").value
+
+const newNode = new Node(id, title, description, button1, button2, button3, button4)
+
+
+const addNode1 = () => {
+
+    for (let i = 0; i < 5; i++) {
+        nodes[0] = new Node(1, "Slide A", "This is description for Slide A", 1, null, null, null)
+    }
+}
+
+
+const addNode2 = () => {
+
+    for (let i = 0; i < 5; i++) {
+        nodes[1] = new Node(2, "Slide B", "This is description for Slide B", null, 2, null, null)
+    }
+}
+
+
+const addNode3 = () => {
+
+    for (let i = 0; i < 5; i++) {
+        nodes[2] = new Node(3, "Slide C", "This is description for Slide C", null, null, 3, null)
+    }
+}
+
+
+const addNode4 = () => {
+
+    for (let i = 0; i < 5; i++) {
+        nodes[3] = new Node(4, "Slide D", "This is description for Slide D", null, null, null, 4)
+    }
+}
+
+console.log(nodes)
+
+/*for (let i = 0; i < 5; i++) {
     nodes[i] = new Node(1, "Slide A", "descriptionSS")
 }
 
