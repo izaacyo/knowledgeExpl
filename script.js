1da6ba35c9c701a7e3567e95447382d74d7f43fe
izaacyo <mariusppetrut@gmail.com>
2021-11-21 09:27:31 +0200

    for loop

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-let button = document.getElementsByClassName("button")
+/*let button = document.getElementsByClassName("button")
 let nodesContainer = document.querySelector("#area")
 let node = document.getElementById("container")
-let t = document.getElementById('target')
+let t = document.getElementById('target')*/
 
-function Node(id, title, description) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.relations = [{
-        button: 1,
-        connection: "",
-        title: ""
-    }, {
-        button: 2,
-        connection: "",
-        title: ""
-    }, {
-        button: 3,
-        connection: "",
-        title: ""
-    }, {
-        button: 4,
-        connection: "",
-        title: ""
-    }]
+let nodes = []
+
+
+class Node {
+    constructor(id, title, description, button, connection, title) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.relations = [{
+            button: button,
+            connection: connection,
+            title: title
+        }, {
+            button: 2,
+            connection: id,
+            title: ""
+        }, {
+            button: 3,
+            connection: id,
+            title: ""
+        }, {
+            button: 4,
+            connection: id,
+            title: ""
+        }]
+    }
+
 }
 
-let nodes = [node];
 
-const addNode = () => {
+for (let i = 0; i < 5; i++) {
+    nodes[i] = new Node(1, "Slide A", "descriptionSS")
+}
+
+
+
+for (let i = 0; i < 1; i++) {
+    (function (i) {
+        this.print = function () {
+            console.log('#' + i + ' ' + this.id
+                + ': ' + this.title + ' ' + this.description + '' + this.relations);
+        }
+        this.print();
+    }).call(nodes[i], i);
+}
+
+
+
+/*const addNode = () => {
 
     if (nodes.length < 5) {
         const newNode = node.cloneNode(true);
@@ -37,24 +60,22 @@ const addNode = () => {
     }
 }
 
-console.log(nodes)
+console.log(nodes)*/
+
+/*let relations = Node["relations"];
+
+for (let i = 0, len = Node.relations.length; i < len; i++) {
+    //  console.log(relations[i])
+    // console.log(relations[i].connection)
+    for (let prop in relations[i]) {
+        console.log(prop, relations[i][prop])
+    }
+}*/
+
 
 
 
 /*
-
-function setup() {
-    for (let i = 0; i < 1; i++) {
-        circles[i] = new Node();
-    }
-}
-
-function draw() {
-    background(0);
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].add()
-    }
-}
 
 function newNode() {
     circles.push(new Node(id, title, description))
@@ -64,7 +85,6 @@ function newNode() {
 function deleteNode() {
     circles.splice()
 }*/
-
 
 
 
